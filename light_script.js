e921486753ca5d0bdffb222c43a18864f82f354e
BogdanKo <bogdan.kondrusik@gmail.com>
2016-08-11 22:04:34 +0300

    div

--- light_script.js
+++ light_script.js
@@ -6,29 +6,29 @@ var LightWidget = (function () {
     var e = new Event('MouseEvents');
 
     this._rootElement = document.querySelector(rootElement),
-    this._btnAdd = this.rootElement.querySelector(.'on_off_btn');
-    this._btn_swtch = this.rootElement.querySelector(.'swtch_orntn');
-    this._swtch_color = this.rootElement.querySelector(.'swtch_color');
+    this._btnAdd = this._rootElement.querySelector(.'on_off_btn');
+    this._btn_swtch = this._rootElement.querySelector(.'swtch_orntn');
+    this._swtch_color = this._rootElement.querySelector(.'swtch_color');
 
 
     Constructor.prototype._handleEvent = function (){
 
-
+                 var self = this;
 
              this._rootElement.addEventListener('click', function (e){
 
-                  if (e.target.classList.contains('')){
-
+                  if (e.target.classList.contains('on_off_btn')){
+                        self.switchOff();
 
                   };
 
-                  if (e.target.classList.contains('on_off_btn')) {
-
+                  if (e.target.classList.contains('swtch_orntn')) {
+                        self.switchOrientation();
 
                   };
 
                   if (e.target.classList.contains('swtch_color')){
-
+                         self.switchColor();
 
 
                   };
@@ -42,16 +42,32 @@ var LightWidget = (function () {
 
 
 
-          };
+      };
 
              Constructor.prototype.switchOrientation = function () {
 
 
-          };
 
-            Constructor.prototype.switchColor = function () {
+      };
+
+             Constructor.prototype.switchColor = function () {
+
+                  function newDiv(div){
+                    this.div = div;
+
+                  };
+
+                  var divGreen = newDiv('green_active');
+                  var dibYellow = newDiv('yellow_active');
+                  var divRed = newDiv('red_active');
+
+                 function () {
 
 
+              }
+
+
+              };
           };
 
 
